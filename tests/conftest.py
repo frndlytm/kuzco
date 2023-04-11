0e0fddc22cf3566e44afb538dedfee30c9619165
Christian DiMare <frndlytm@gmail.com>
2023-04-10 23:57:46 -0400

    Test filters and transformers.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,19 @@
 import pytest
 
+import kuzco
 
+
+@pytest.fixture
+def worlds():
+    yield kuzco.chan(
+        *[
+            {"id": 1, "world": "Mercury"},
+            {"id": 2, "world": "Venus"},
+            {"id": 3, "world": "Earth"},
+            {"id": 4, "world": "Mars"},
+            {"id": 5, "world": "Jupiter"},
+            {"id": 6, "world": "Saturn"},
+            {"id": 7, "world": "Uranus"},
+            {"id": 8, "world": "Neptune"},
+        ]
+    )
--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,3 +1,44 @@
+import pytest
+
+import kuzco
+from kuzco.filters import Exclude, Flatten, Include, Map
 
 
+def test_exclude(worlds):
+    assert (
+        list(Exclude(lambda m: m.get("id") > 3)(worlds)) == [
+            {"id": 1, "world": "Mercury"},
+            {"id": 2, "world": "Venus"},
+            {"id": 3, "world": "Earth"},
+        ]
+    )
 
+
+def test_include(worlds):
+    assert (
+        list(Include(lambda m: m.get("id") <= 3)(worlds)) == [
+            {"id": 1, "world": "Mercury"},
+            {"id": 2, "world": "Venus"},
+            {"id": 3, "world": "Earth"},
+        ]
+    )
+
+
+def test_map(worlds):
+    assert (
+        list(Map(lambda m: {"msg": f"Hello, {m.get('world')}!"})(worlds)) == [
+            {"msg": "Hello, Mercury!"},
+            {"msg": "Hello, Venus!"},
+            {"msg": "Hello, Earth!"},
+            {"msg": "Hello, Mars!"},
+            {"msg": "Hello, Jupiter!"},
+            {"msg": "Hello, Saturn!"},
+            {"msg": "Hello, Uranus!"},
+            {"msg": "Hello, Neptune!"},
+        ]
+    )
+
+
+def test_flatten(worlds):
+    data = list(worlds)
+    assert list(Flatten()([data, data])) == [*data, *data]
